b7c303331c4aa2931da89a740197c9cc19145a25
pompz-bit <danduartes@gmail.com>
2025-06-16 23:56:42 +0100

    fix compile script doesn't require Wx

--- src/build/compile.bat
+++ src/build/compile.bat
@@ -1,7 +1,7 @@
 cd ..\..
 REM Build start.exe using PAR::Packer (pp) instead of the discontinued PerlApp
 pp --icon src\build\openkore.ico -I src -o start.exe start.pl ^
-   -M Pod::Usage -M I18N::Langinfo -M Wx -M "Wx::*" -M XSTools ^
+   -M Pod::Usage -M I18N::Langinfo -M XSTools ^
    -M List::Util -M File::Path -M Text::Balanced -M Digest::MD5 ^
    -M Math::BigInt -M Math::BigInt::Calc -M Math::BigInt::CalcEmu ^
    -M Math::BigInt::FastCalc -M Math::BigInt::Trace -M Math::BigFloat ^
